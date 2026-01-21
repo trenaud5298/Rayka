163ec666c4aa2127b8a63392b2710023ce0c376e
Tyler Renaud <trenaud_5298@email.ric.edu>
2026-01-20 21:36:00 -0500

    Finished Vec2 and Vec3 classes.

--- Core/include/Rayka/Core/Math/Vec2.h
+++ Core/include/Rayka/Core/Math/Vec2.h
@@ -1,10 +1,47 @@
 #ifndef RAYKA_CORE_VEC2_H
 #define RAYKA_CORE_VEC2_H
 
+// Rayka Includes
+#include <Rayka/Core/Export.h>
+
+// C++ Includes
+#include <cmath>
+
 namespace Rayka {
-    struct Vec2 {
+    struct RAYKA_CORE_EXPORT Vec2 {
         float x, y;
+
+        // Constructors
+        constexpr Vec2() noexcept : x(0.0f), y(0.0f) {}
+        constexpr Vec2(float v) noexcept : x(v), y(v) {}
+        constexpr Vec2(float x, float y) noexcept : x(x), y(y) {}
+
+        // Operators (Self)
+        constexpr Vec2 operator-() const noexcept { return {-x, -y}; }
+        constexpr Vec2& operator+=(Vec2 v) noexcept { x += v.x; y += v.y; return *this; }
+        constexpr Vec2& operator-=(Vec2 v) noexcept { x -= v.x; y -= v.y; return *this; }
+        constexpr Vec2& operator*=(float s) noexcept { x *= s; y *= s; return *this; }
+        constexpr Vec2& operator/=(float s) noexcept { return *this *= (1.0f / s); } // Undefined behavior for s == 0 (matches IEEE-754 behavior)
+
+        // Math Functions
+        [[nodiscard]] constexpr float norm2() const noexcept { return x*x + y*y; }
+        [[nodiscard]] float norm() const noexcept { return std::sqrt(norm2()); }
+        [[nodiscard]] float invNorm() const noexcept { return 1.0f / norm(); }
+        [[nodiscard]] Vec2 normalized() const noexcept { float inv = invNorm(); return {x*inv, y*inv}; }
+        Vec2& normalize() noexcept { return *this *= invNorm(); }
     };
+
+    // Free Operators
+    [[nodiscard]] constexpr Vec2 operator+(Vec2 a, Vec2 b) noexcept { return {a.x + b.x, a.y + b.y}; }
+    [[nodiscard]] constexpr Vec2 operator-(Vec2 a, Vec2 b) noexcept { return {a.x -b.x, a.y - b.y}; }
+    [[nodiscard]] constexpr Vec2 operator*(Vec2 v, float s) noexcept { return {v.x * s, v.y * s}; }
+    [[nodiscard]] constexpr Vec2 operator*(float s, Vec2 v) noexcept { return {v.x * s, v.y * s}; }
+    [[nodiscard]] constexpr Vec2 operator*(Vec2 a, Vec2 b) noexcept { return {a.x*b.x, a.y*b.y}; }
+    [[nodiscard]] constexpr Vec2 operator/(Vec2 v, float s) noexcept { return Vec2(v)/=s; }
+
+    // Free Math Functions
+    [[nodiscard]] constexpr float dot(Vec2 a, Vec2 b) noexcept { return a.x*b.x + a.y*b.y; }
+    [[nodiscard]] inline bool nearlyEqual(Vec2 a, Vec2 b, float eps = 1e-6f) noexcept { return std::fabs(a.x-b.x) < eps && std::fabs(a.y-b.y) < eps; }
 }
 
 #endif
--- Core/include/Rayka/Core/Math/Vec3.h
+++ Core/include/Rayka/Core/Math/Vec3.h
@@ -1,9 +1,15 @@
 #ifndef RAYKA_CORE_VEC3_H
 #define RAYKA_CORE_VEC3_H
 
+// Rayka Includes
+#include <Rayka/Core/Export.h>
+
+// C++ Includes
+#include <cmath>
+
 namespace Rayka {
-    struct Vec3 {
-        float x,y,z;
+    struct RAYKA_CORE_EXPORT Vec3 {
+        float x, y, z;
 
         // Constructors
         constexpr Vec3() noexcept : x(0.0f), y(0.0f), z(0.0f) {}
@@ -13,20 +19,30 @@ namespace Rayka {
         // Operators (Self)
         constexpr Vec3 operator-() const noexcept { return {-x, -y, -z}; }
         constexpr Vec3& operator+=(Vec3 v) noexcept { x += v.x; y += v.y; z += v.z; return *this; }
-        constexpr Vec3& operator-=(Vec3 v) noexcept { x -= v.x; y += v.y; z += v.z; return *this; }
+        constexpr Vec3& operator-=(Vec3 v) noexcept { x -= v.x; y -= v.y; z -= v.z; return *this; }
         constexpr Vec3& operator*=(float s) noexcept { x *= s; y *= s; z *= s; return *this; }
+        constexpr Vec3& operator/=(float s) noexcept { return *this *= (1.0f / s); } // Undefined behavior for s == 0 (matches IEEE-754 behavior)
 
+        // Math Functions
+        [[nodiscard]] constexpr float norm2() const noexcept { return x*x + y*y + z*z; }
+        [[nodiscard]] float norm() const noexcept { return std::sqrt(norm2()); }
+        [[nodiscard]] float invNorm() const noexcept { return 1.0f / norm(); }
+        [[nodiscard]] Vec3 normalized() const noexcept { float inv = invNorm(); return {x*inv, y*inv, z*inv}; }
+        Vec3& normalize() noexcept { return *this *= invNorm(); }
     };
 
     // Free Operators
-    constexpr Vec3 operator+(Vec3 a, Vec3 b) noexcept { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
-    constexpr Vec3 operator-(Vec3 a, Vec3 b) noexcept { return {a.x -b.x, a.y - b.y, a.z - b.z}; }
-    constexpr Vec3 operator*(Vec3 v, float s) noexcept { return {v.x * s, v.y * s, v.z * s}; }
-    constexpr Vec3 operator*(float s, Vec3 v) noexcept { return {v.x * s, v.y * s, v.z * s}; }
+    [[nodiscard]] constexpr Vec3 operator+(Vec3 a, Vec3 b) noexcept { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
+    [[nodiscard]] constexpr Vec3 operator-(Vec3 a,  Vec3 b) noexcept { return {a.x -b.x, a.y - b.y, a.z - b.z}; }
+    [[nodiscard]] constexpr Vec3 operator*(Vec3 v, float s) noexcept { return {v.x * s, v.y * s, v.z * s}; }
+    [[nodiscard]] constexpr Vec3 operator*(float s, Vec3 v) noexcept { return {v.x * s, v.y * s, v.z * s}; }
+    [[nodiscard]] constexpr Vec3 operator*(Vec3 a, Vec3 b) noexcept { return {a.x*b.x, a.y*b.y, a.z*b.z}; }
+    [[nodiscard]] constexpr Vec3 operator/(Vec3 v, float s) noexcept { return Vec3(v)/=s; }
 
-    // Math Functions
-    constexpr float dot(Vec3 a, Vec3 b) noexcept { return a.x*b.x + a.y*b.y + a.z*b.z; }
-    constexpr Vec3 cross(Vec3 a, Vec3 b) noexcept { return {a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x}; }
+    // Free Math Functions
+    [[nodiscard]] constexpr float dot(Vec3 a, Vec3 b) noexcept { return a.x*b.x + a.y*b.y + a.z*b.z; }
+    [[nodiscard]] constexpr Vec3 cross(Vec3 a, Vec3 b) noexcept { return {a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x}; }
+    [[nodiscard]] inline bool nearlyEqual(Vec3 a, Vec3 b, float eps = 1e-6f) noexcept { return std::fabs(a.x-b.x) < eps && std::fabs(a.y-b.y) < eps && std::fabs(a.z-b.z) < eps; }
 }
 
 #endif
